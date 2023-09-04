75a5863c0ddd015929f4233fe49fe23ca35a3641
Rodrigo Cunha Rodrigues <wkm.rcunha@terceiros.mag.com.br>
2023-09-04 20:51:49 -0300

    feat(Dashboard): commit parcial - cria modulo dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  {
     path: 'orders',
     loadChildren: () => import('./pages/orders/orders.module').then(m => m.OrdersModule)
   },
