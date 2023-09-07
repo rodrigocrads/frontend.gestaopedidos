c63e46c4d210c920e49a4530f65f94ea7e055548
Rodrigo Cunha Rodrigues <wkm.rcunha@terceiros.mag.com.br>
2023-09-07 13:36:34 -0300

    feat(Dashboard): partial commit - adicionando mock do chart

--- src/app/pages/dashboard/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  ngOnInit(): void {
+    new Chart("myChart", {
+      type: 'bar',
+      data: {
+        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        datasets: [{
+          label: 'Faturamento',
+          data: [1500, 1000, 3000, 1500, 1200, 3000, 1100, 1300, 1400, 1200, 1500],
+          backgroundColor: [
+            'rgba(75, 192, 192, 0.2)',
 
+          ],
+          borderColor: [
+            'rgb(75, 192, 192)',
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 }
