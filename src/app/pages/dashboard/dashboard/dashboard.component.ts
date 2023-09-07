b4413ee53cb03e52f623afd5782d6ff4fdeca261
Rodrigo Cunha Rodrigues <wkm.rcunha@terceiros.mag.com.br>
2023-09-07 17:45:17 -0300

    feat(Dashboard): cria logica para exibição dos dados de forma dinamica

--- src/app/pages/dashboard/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../orders/shared/order.model';
+import { OrderService } from '../../orders/shared/order.service';
+
 import { Chart, registerables } from 'chart.js';
+import { OrderStatus } from '../../orders/shared/order-status.enum';
+import * as moment from 'moment';
 Chart.register(...registerables);
 
 @Component({
@@ -8,17 +13,66 @@ Chart.register(...registerables);
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+  orders: Order[] = [];
+  goalOfMonth = 1500;
+  chartData: object[] = [];
+
+  constructor(
+    private orderService: OrderService
+  ) {}
+
   ngOnInit(): void {
+    this.orderService.listAll()
+      .subscribe({
+        next: orders => {
+          this.orders = orders;
+          this.loadChart();
+        }
+      });
+  }
+
+  public calculateInvoincing(): number {
+    const orders = this.getOrdersByStatus(OrderStatus.CONCLUIDO)
+    return this.sumOrdersTotal(orders);
+  }
+
+  public calculateGoalDiffPercentage(): number {
+    return Math.round(this.calculateInvoincing() * 100 / this.goalOfMonth - 100);
+  }
+
+  public waitingConfirmationLength(): number {
+    return this.getOrdersByStatus(OrderStatus.AGUARDANDO_CONFIRMACAO).length;
+  }
+
+  public confirmedLength(): number {
+    return this.getOrdersByStatus(OrderStatus.CONFIRMADO).length;
+  }
+
+  public pendingLength(): number {
+    return this.getOrdersByStatus(OrderStatus.PENDENTE).length;
+  }
+
+  public completedLength(): number {
+    return this.getOrdersByStatus(OrderStatus.CONCLUIDO).length;
+  }
+
+  private getOrdersByStatus(status: OrderStatus): Order[] {
+    return this.orders.filter(order => order.status === status);
+  }
+
+  private loadChart() {
+    const labels = this.getChartLabels();    
+
     new Chart("myChart", {
       type: 'bar',
       data: {
-        labels: ['Jan', 'Fev', 'Mar', 'Abr', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+        labels: this.chartData.map((data: any) => data.label),
         datasets: [{
           label: 'Faturamento',
-          data: [1500, 1000, 3000, 1500, 1200, 3000, 1100, 1300, 1400, 1200, 1500],
+          data: this.chartData.map((data: any) => data.total),
           backgroundColor: [
             'rgba(75, 192, 192, 0.2)',
-
           ],
           borderColor: [
             'rgb(75, 192, 192)',
@@ -34,5 +88,48 @@ export class DashboardComponent implements OnInit {
         }
       }
     });
+  }
+
+  private getChartLabels() {
+    const monthsNames = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+    let monthIndex = new Date().getMonth() -1;
+    let year = new Date().getFullYear();
+
+    for (let i=0; i<=11; i++) {
+
+      this.chartData.push({
+        label: `${monthsNames[monthIndex]}/${year}`,
+        total: this.sumOrdersTotal(this.filterOrdersByDateAndCompleted(monthIndex, year))
+      });
+
+      if (monthIndex === 0) {
+        monthIndex = 11;
+        year--;
+        continue;
+      }
+
+      monthIndex--;
+    }
+    
+    this.chartData = this.chartData.reverse();
+    console.log(this.chartData);
+  }
+
+  private filterOrdersByDateAndCompleted(month: number, year: number) {
+    return this.orders.filter(order => {
+      const orderDate = moment(order.deliveryDate, 'DD/MM/YYYY');
+
+      const monthMatches = orderDate.month() == month;
+      const yearMatches = orderDate.year() == year;
+
+      return monthMatches && yearMatches;
+    });
+  }
+
+  private sumOrdersTotal(orders: Order[]): number {
+    return orders.reduce((acc: number, curr: Order) => {
+      const value = `${curr?.total}`.replace('.', '').replace(',', '.');
+      return acc + (parseFloat(value) || 0);
+    }, 0);
   }
 }
